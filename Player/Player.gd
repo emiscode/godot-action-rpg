33e664ecfc248b70c274507346f2aef74ed5b22f
Emilio Scodeler <emiscode@gmail.com>
2021-08-25 00:04:19 -0300

    Added collision shapes

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,30 +1,29 @@
 extends KinematicBody2D
 
-const FRICTION = 10
-const MAX_SPEED = 100
-const ACCELERATION = 10
+export var speed = 150
+export var friction = 6
+export var acceleration = 3
 
-var velocity = Vector2.ZERO
+var velocity = Vector2()
 
-func _physics_process(delta):
-	handle_input(delta)
-	move_and_collide(velocity)
-	
-func handle_input(delta):
-	var input_vector = Vector2.ZERO
+func get_input():
+	var input = Vector2.ZERO
 	var input_right = Input.get_action_strength("ui_right")
 	var input_left = Input.get_action_strength("ui_left")
 	var input_up = Input.get_action_strength("ui_up")
 	var input_down = Input.get_action_strength("ui_down")
 	
-	input_vector.x = input_right - input_left
-	input_vector.y = input_down - input_up
+	input.x = input_right - input_left
+	input.y = input_down - input_up
 	
-	if input_vector != Vector2.ZERO:
-		velocity = input_vector.normalized() * MAX_SPEED * delta
-		#velocity += input_vector.normalized() * ACCELERATION * delta		
-		#velocity = velocity.clamped(MAX_SPEED * delta)
+	return input
+
+func _physics_process(delta):
+	var direction = get_input()
+	
+	if direction != Vector2.ZERO:
+		velocity = lerp(velocity, direction.normalized() * speed, acceleration * delta)
 	else:
-		velocity = Vector2.ZERO
-		#velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-		
+		velocity = lerp(velocity, Vector2.ZERO, friction * delta)
+	
+	velocity = move_and_slide(velocity)
